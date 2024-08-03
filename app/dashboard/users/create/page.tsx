55321e81cff58e0bc27590e6a5729cead24ee3bb
jose1193 <josegonzalezcr2794@gmail.com>
2024-08-03 02:57:05 +0100

    DELETE: Role Guard Component

--- app/dashboard/users/create/page.tsx
+++ app/dashboard/users/create/page.tsx
@@ -9,7 +9,8 @@ import { Container, Typography, Box, Paper } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { UserData } from "../../../../app/types/user";
 import { useSession } from "next-auth/react";
-import RoleGuard from "@/components/RoleGuard";
+import { withRoleProtection } from "../../../../src/components/withRoleProtection";
+
 const CreateUserPage = () => {
   const { data: session, update } = useSession();
   const router = useRouter();
@@ -24,50 +25,44 @@ const CreateUserPage = () => {
   };
 
   return (
-    <RoleGuard
-      allowedRoles={["Super Admin", "Admin"]}
-      redirectTo="/unauthorized"
-    >
-      <Suspense>
-        <Box
-          sx={{
-            flexGrow: 1,
-            overflow: "hidden",
+    <Suspense>
+      <Box
+        sx={{
+          flexGrow: 1,
+          overflow: "hidden",
 
-            ml: -7,
-            mb: 10,
-            p: { xs: 3, sm: 3, md: 2, lg: 4 },
+          ml: -7,
+          mb: 10,
+          p: { xs: 3, sm: 3, md: 2, lg: 4 },
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="h1"
+          gutterBottom
+          sx={{
+            mb: 5,
+            fontSize: {
+              xs: "1.5rem",
+              sm: "1.75rem",
+              md: "2rem",
+              lg: "2.25rem",
+            },
           }}
         >
-          <Typography
-            variant="h4"
-            component="h1"
-            gutterBottom
-            sx={{
-              mb: 5,
-              fontSize: {
-                xs: "1.5rem",
-                sm: "1.75rem",
-                md: "2rem",
-                lg: "2.25rem",
-              },
-            }}
-          >
-            Create User
-          </Typography>
-          <Paper
-            elevation={3}
-            style={{
-              padding: "20px",
-              border: "1px solid rgba(255, 255, 255, 0.2)",
-            }}
-          >
-            <UsersForm onSubmit={handleSubmit} />
-          </Paper>
-        </Box>
-      </Suspense>
-    </RoleGuard>
+          Create User
+        </Typography>
+        <Paper
+          elevation={3}
+          style={{
+            padding: "20px",
+            border: "1px solid rgba(255, 255, 255, 0.2)",
+          }}
+        >
+          <UsersForm onSubmit={handleSubmit} />
+        </Paper>
+      </Box>
+    </Suspense>
   );
 };
-
-export default CreateUserPage;
+export default withRoleProtection(CreateUserPage, ["Super Admin"]);
