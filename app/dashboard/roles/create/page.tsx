f5974d212508f3b1213556ac1617ae6965d6e94d
jose1193 <josegonzalezcr2794@gmail.com>
2024-08-03 02:53:49 +0100

    DELETE: Role Guard Component

--- app/dashboard/roles/create/page.tsx
+++ app/dashboard/roles/create/page.tsx
@@ -9,7 +9,7 @@ import { Typography, Box, Paper } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { RolesData } from "../../../../app/types/roles";
 import { useSession } from "next-auth/react";
-import RoleGuard from "@/components/RoleGuard";
+import { withRoleProtection } from "../../../../src/components/withRoleProtection";
 
 const CreateRolePage = () => {
   const { data: session } = useSession();
@@ -25,49 +25,43 @@ const CreateRolePage = () => {
   };
 
   return (
-    <RoleGuard
-      allowedRoles={["Super Admin", "Admin"]}
-      redirectTo="/unauthorized"
-    >
-      <Suspense>
-        <Box
+    <Suspense>
+      <Box
+        sx={{
+          flexGrow: 1,
+          overflow: "hidden",
+          ml: -7,
+          mb: 10,
+          p: { xs: 3, sm: 3, md: 2, lg: 4 },
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="h1"
+          gutterBottom
           sx={{
-            flexGrow: 1,
-            overflow: "hidden",
-            ml: -7,
-            mb: 10,
-            p: { xs: 3, sm: 3, md: 2, lg: 4 },
+            mb: 5,
+            fontSize: {
+              xs: "1.5rem",
+              sm: "1.75rem",
+              md: "2rem",
+              lg: "2.25rem",
+            },
           }}
         >
-          <Typography
-            variant="h4"
-            component="h1"
-            gutterBottom
-            sx={{
-              mb: 5,
-              fontSize: {
-                xs: "1.5rem",
-                sm: "1.75rem",
-                md: "2rem",
-                lg: "2.25rem",
-              },
-            }}
-          >
-            Create Role
-          </Typography>
-          <Paper
-            elevation={3}
-            style={{
-              padding: "20px",
-              border: "1px solid rgba(255, 255, 255, 0.2)",
-            }}
-          >
-            <RoleForm onSubmit={handleSubmit} />
-          </Paper>
-        </Box>
-      </Suspense>
-    </RoleGuard>
+          Create Role
+        </Typography>
+        <Paper
+          elevation={3}
+          style={{
+            padding: "20px",
+            border: "1px solid rgba(255, 255, 255, 0.2)",
+          }}
+        >
+          <RoleForm onSubmit={handleSubmit} />
+        </Paper>
+      </Box>
+    </Suspense>
   );
 };
-
-export default CreateRolePage;
+export default withRoleProtection(CreateRolePage, ["Super Admin"]);
